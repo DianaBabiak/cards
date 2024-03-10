7af6da6353a51ffe14a061f515b53b1e9e2262cf
BYAsurim <Pers0naL276276@gmail.com>
2024-03-10 20:44:13 +0300

    refactor checkbox

--- src/components/ui/checkBox/Checkbox.tsx
+++ src/components/ui/checkBox/Checkbox.tsx
@@ -1,37 +1,40 @@
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
+
+import { Typography } from '@/components/ui/typography'
 import * as RadixCheckbox from '@radix-ui/react-checkbox'
 import { CheckIcon } from '@radix-ui/react-icons'
 
 import s from './checkBox.module.scss'
 
-type Props = {
-  checked: boolean
-  className?: string
-  disabled?: boolean
-  id?: string
+export type Props = ComponentPropsWithoutRef<typeof RadixCheckbox.Root> & {
   label?: string
-  onChange: (checked: boolean) => void
+  onChange?: (checked: boolean) => void
 }
-export const Checkbox = ({ checked, className, disabled, id, label, onChange }: Props) => {
-  const classNames = `${s.checkboxContainer} ${className}`
+export const Checkbox = forwardRef<ElementRef<typeof RadixCheckbox.Root>, Props>(
+  ({ checked, className, disabled, id, label, onChange, ...rest }, ref) => {
+    const classNames = `${s.checkboxContainer} ${className}`
 
-  return (
-    <div className={classNames}>
-      <RadixCheckbox.Root
-        checked={checked}
-        className={s.checkBox}
-        disabled={disabled}
-        id={id}
-        onCheckedChange={onChange}
-      >
-        <RadixCheckbox.Indicator className={s.checkboxIndicator}>
-          <CheckIcon />
-        </RadixCheckbox.Indicator>
-      </RadixCheckbox.Root>
-      {label && (
-        <label className={s.label} htmlFor={'c1'}>
-          {label}
-        </label>
-      )}
-    </div>
-  )
-}
+    return (
+      <div className={classNames}>
+        <RadixCheckbox.Root
+          checked={checked}
+          className={s.checkBox}
+          disabled={disabled}
+          id={id}
+          onCheckedChange={onChange}
+          ref={ref}
+          {...rest}
+        >
+          <RadixCheckbox.Indicator className={s.checkboxIndicator}>
+            <CheckIcon />
+          </RadixCheckbox.Indicator>
+        </RadixCheckbox.Root>
+        {label && (
+          <Typography as={'label'} className={s.label} variant={'body2'}>
+            {label}
+          </Typography>
+        )}
+      </div>
+    )
+  }
+)
