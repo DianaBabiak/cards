f14fd8d40057db07f9bb80e625753eb6a51d98a9
kbabiak <kirill.babiak@carrier.com>
2024-03-16 22:38:46 +0100

    refactoring Modal

--- src/components/ui/modal/contentContainerModal/cardModal/itemCardModal/ItemCardModal.tsx
+++ src/components/ui/modal/contentContainerModal/cardModal/itemCardModal/ItemCardModal.tsx
@@ -9,44 +9,51 @@ import s from '@/components/ui/modal/modal.module.scss'
 
 export type ItemCardModalProps = {
   label: string
+  placeholder?: string
 }
 
-export const ItemCardModal = forwardRef<HTMLDivElement, ItemCardModalProps>(({ label }, ref) => {
-  const [selectedImage, setSelectedImage] = useState(imgReact)
-  const inputRef = createRef<HTMLInputElement>()
-  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files ? e.target.files[0] : null
+export const ItemCardModal = forwardRef<HTMLDivElement, ItemCardModalProps>(
+  ({ label, placeholder }, ref) => {
+    const [selectedImage, setSelectedImage] = useState(imgReact)
+    const inputRef = createRef<HTMLInputElement>()
+    const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+      const file = e.target.files ? e.target.files[0] : null
 
-    if (file) {
-      const reader = new FileReader()
+      if (file) {
+        const reader = new FileReader()
 
-      reader.onloadend = () => {
-        setSelectedImage(reader.result as string)
+        reader.onloadend = () => {
+          setSelectedImage(reader.result as string)
+        }
+        reader.readAsDataURL(file)
       }
-      reader.readAsDataURL(file)
     }
-  }
 
-  return (
-    <div className={s.wrapperItemCard} ref={ref}>
-      <Typography variant={'subtitle2'}>{label}:</Typography>
-      <TextField className={s.hundredPercentWidth} label={' Question?'} />
-      <div className={s.wrapperImgContent}>
-        <img alt={label} src={selectedImage} />
-        <label className={s.customFileInput}>
-          <input
-            accept={'image/*'}
-            className={s.coverInput}
-            onChange={handleImageChange}
-            ref={inputRef}
-            type={'file'}
-          />
-          <div className={s.fileInputContent}>
-            <Icon iconId={'chooseFile'} />
-            <Typography variant={'subtitle2'}>Change Cover</Typography>
-          </div>
-        </label>
+    return (
+      <div className={s.wrapperItemCard} ref={ref}>
+        <Typography variant={'subtitle2'}>{label}:</Typography>
+        <TextField
+          className={s.hundredPercentWidth}
+          label={' Question?'}
+          placeholder={placeholder}
+        />
+        <div className={s.wrapperImgContent}>
+          <img alt={label} src={selectedImage} />
+          <label className={s.customFileInput}>
+            <input
+              accept={'image/*'}
+              className={s.coverInput}
+              onChange={handleImageChange}
+              ref={inputRef}
+              type={'file'}
+            />
+            <div className={s.fileInputContent}>
+              <Icon iconId={'chooseFile'} />
+              <Typography variant={'subtitle2'}>Change Cover</Typography>
+            </div>
+          </label>
+        </div>
       </div>
-    </div>
-  )
-})
+    )
+  }
+)
