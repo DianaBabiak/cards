c0b7648147dc854be3c97e41b91e6894257f0199
kbabiak <kirill.babiak@carrier.com>
2024-03-30 17:22:08 +0100

    change reset in LearnDeckForm

--- src/features/learnDeck/LearnDeck.tsx
+++ src/features/learnDeck/LearnDeck.tsx
@@ -76,7 +76,7 @@ export const LearnDeck = ({}: LearnDeckProps) => {
         idDeck: idDeck || '',
       })
       if (!isError) {
-        reset()
+        reset({ grade: '1' })
         setIsShowAnswer(false)
       }
     } catch (err) {
